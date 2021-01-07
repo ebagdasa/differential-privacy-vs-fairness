e2ce8bae8a61b522388c4343a2de68fd0f7021a5
jpgard <joshua.patrick.gardner@gmail.com>
2021-01-07 14:09:07 -0800

    convert numpy to torch in Dataset getter function.

--- utils/celeba_dataset.py
+++ utils/celeba_dataset.py
@@ -48,6 +48,7 @@ class CelebADataset(torch.utils.data.Dataset):
         img_name = os.path.join(self.root_dir,
                                 self.anno.index[idx])
         image = io.imread(img_name)
+        image = torch.from_numpy(image)
         anno = self.anno.iloc[idx, :].astype(float).to_dict()
 
         if self.transform:
