d7d4cd8f86c3a5a9514df1af149ec2a28c0700b5
jpgard <joshua.patrick.gardner@gmail.com>
2021-02-03 10:16:24 -0800

    initial commit

---
+++ params/params_cifar_nodp.yaml.py
@@ -0,0 +1,30 @@
+test_batch_size: 100
+batch_size: 256
+num_microbatches: 256
+lr: 0.01
+momentum: 0.5
+decay: 0
+dp: False
+epochs: 120
+save_on_epochs: [10, 50, 100, 110]
+
+mu: 1.0
+
+ds_size: 11500
+
+# Note: 0 = airplane, 3 = cat, 5 = dog, 8 = ship
+minority_group_keys: [3, 5]  # Should be a subset of the positive and/or negative class keys.
+positive_class_keys: [0, 3]  # These keys are grouped into the class with label 1.
+negative_class_keys: [5, 8]  # These keys are grouped into the class with label 0.
+fixed_n_train: 11500
+number_of_entries_test: 1000
+optimizer: SGD
+
+save_model: True
+dataset: cifar10
+model: net
+scheduler: False
+
+count_norm_cosine_per_batch: False
+
+csigma: 0.9
