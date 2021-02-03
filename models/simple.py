552e7a400c9c03580523d7b88fb219c87b007b34
jpgard <joshua.patrick.gardner@gmail.com>
2021-02-03 11:09:27 -0800

    remove commented code

--- models/simple.py
+++ models/simple.py
@@ -131,19 +131,3 @@ class FlexiNet(SimpleNet):
             x = self.fc2(x)
             return F.log_softmax(x, dim=1)
 
-
-#
-  # input_layer = tf.reshape(features['x'], [-1, 28, 28, 1])
-  # y = tf.keras.layers.Conv2D(16, 8,
-  #                            strides=2,
-  #                            padding='same',
-  #                            activation='relu').apply(input_layer)
-  # y = tf.keras.layers.MaxPool2D(2, 1).apply(y)
-  # y = tf.keras.layers.Conv2D(32, 4,
-  #                            strides=2,
-  #                            padding='valid',
-  #                            activation='relu').apply(y)
-  # y = tf.keras.layers.MaxPool2D(2, 1).apply(y)
-  # y = tf.keras.layers.Flatten().apply(y)
-  # y = tf.keras.layers.Dense(32, activation='relu').apply(y)
-  # logits = tf.keras.layers.Dense(10).apply(y)
