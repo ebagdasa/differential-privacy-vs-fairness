75d75134f441a4ecf9089799ac57585194647a26
jpgard <joshua.patrick.gardner@gmail.com>
2021-05-14 16:29:24 -0700

    remove print statements

--- dpdi/datasets/law.py
+++ dpdi/datasets/law.py
@@ -17,9 +17,7 @@ def load_law_dataset(sensitive, root_dir="../data/ucla-law-school"):
         'lsat', 'ugpa', 'zfygpa', 'dob_yr', 'zgpa', 'bar1', 'bar1_yr',
         'bar2', 'bar2_yr', 'fam_inc', 'parttime',
         'pass_bar', 'bar', 'tier']]
-    print(data.shape)
     data = pd.get_dummies(data)
-    print(data['zgpa'].describe())
     if sensitive == 'sex':
         data['sex'] -= 1
         data.rename(columns={'sex': 'sensitive'}, inplace=True)
@@ -31,6 +29,5 @@ def load_law_dataset(sensitive, root_dir="../data/ucla-law-school"):
     data.rename(columns={'zgpa': 'target'}, inplace=True)
     for x in data.columns:
         if x != 'sensitive':
-            print(x, np.std(data[x]))
             data[x] = (data[x] - np.mean(data[x])) / np.std(data[x])
     return data
