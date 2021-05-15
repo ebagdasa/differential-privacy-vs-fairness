26c743566f58c255fc0819bb8d7fa13f1ee54f68
jpgard <joshua.patrick.gardner@gmail.com>
2021-05-15 13:00:20 -0700

    very incomplete initial implementation of only the get_anno_df() function.

--- dpdi/datasets/zillow_dataset.py
+++ dpdi/datasets/zillow_dataset.py
@@ -4,10 +4,14 @@ import os
 import numpy as np
 from torchvision.datasets.folder import default_loader
 from torchvision import transforms
+import time
+
+import pandas as pd
 
 
 def get_anno_df(root_dir):
     df = pd.read_pickle(os.path.join(root_dir, "df_pickle4.pkl"))
-    df["image_location"] = df["zpid"].apply(
-        lambda x:os.path.join(root_dir, "processed_images", x, ".png"))
+    df["image_fp"] = df["zpid"].apply(
+        lambda x: os.path.join(root_dir, "processed_images", x, ".png"))
+
     return df
