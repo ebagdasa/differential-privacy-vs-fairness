70ab45f68b3e146cafba74d3549177bc3f6204a3
jpgard <joshua.patrick.gardner@gmail.com>
2021-05-17 11:08:25 -0700

    bug fix: remove call to torch.from_numpy

--- dpdi/datasets/zillow_dataset.py
+++ dpdi/datasets/zillow_dataset.py
@@ -70,7 +70,7 @@ class ZillowDataset(Dataset):
             idx = idx.tolist()
         fp = self.anno["img_fp"].values[idx]
         image = self.loader(fp)
-        label = torch.from_numpy(self.targets[idx])
+        label = self.targets[idx]
         if self.transform:
             image = self.transform(image)
         sample = (image, idx, label)
