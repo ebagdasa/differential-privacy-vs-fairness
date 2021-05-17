1cda74a5508c5b6e2ca248eabc15dfa63ff3932e
jpgard <joshua.patrick.gardner@gmail.com>
2021-05-16 19:44:53 -0700

    updates to plotting and data reading functions

--- rscripts/convergence_rates_and_bias.R
+++ rscripts/convergence_rates_and_bias.R
@@ -3,14 +3,14 @@ source("utils.R")
 
 test_class_levels = c("0" = TeX("Loss on Minority (L_0)"), 
                       "1" = TeX("Loss on Majority (L_1)"))
-dp_str_levels = c("No DP" = TeX("No DP"),
-                  "DP-SGD, S = 1, z = 0.8"= TeX("DP-SGD, $\\beta = 1,  \\sigma_{DP}^2 = 0.8$"),
-                  "DP-SGD, S = 1, z = 1" = TeX("DP-SGD, $\\beta = 1,  \\sigma_{DP}^2 = 1.0$"))
+# dp_str_levels = c("No DP" = TeX("No DP"),
+#                   "DP-SGD, S = 1, z = 0.8"= TeX("DP-SGD, $\\beta = 1,  \\sigma_{DP}^2 = 0.8$"),
+#                   "DP-SGD, S = 1, z = 1" = TeX("DP-SGD, $\\beta = 1,  \\sigma_{DP}^2 = 1.0$"))
 
 mnist_convergence <- read_convergence_data("./data/mnist-grouped", "attr")
 mnist_convergence$test_class = factor(mnist_convergence$test_class)
 levels(mnist_convergence$test_class) <- test_class_levels
-levels(mnist_convergence$dp_str) <- dp_str_levels
+# levels(mnist_convergence$dp_str) <- dp_str_levels
 
 convergence_plot_theme <- theme(plot.title=element_text(hjust=0.5, size=rel(1.5)),
                                 strip.text = element_text(size=rel(0.9)), # size of the facet label text
@@ -32,11 +32,13 @@ loss_plot_title <- ggtitle(TeX("Loss $L_j(\\hat{w})$ vs. Majority Fraction $\\al
 alpha_label_name = unname(TeX("Majority Fraction $\\alpha$"))
 
 mnist_convergence %>%
+  # dplyr::filter(dp_str=="DPFALSESNAzNAsigmaNA" | dp_str=="DPTRUESInfzNAsigma50") %>%
   ggplot(aes(x=Step, y=Value, group=factor(alpha))) +
-  geom_line(aes(col=alpha)) +
+  geom_line(aes(col=as.numeric(alpha))) +
   facet_grid(rows = vars(factor(test_class)),
              cols = vars(dp_str),
-             labeller=label_parsed) +
+             # labeller=label_parsed
+             ) +
   theme_bw() +
   xlab("Epoch") +
   ylab("Loss") +
@@ -51,31 +53,52 @@ mnist_convergence %>%
 ggsave("./convergence_behavior_mnist_grouped.pdf", device="pdf", height=7, width=12)  
 
 p1 <- mnist_convergence %>%
-  dplyr::filter(as.integer(dp_str) < 3) %>%
+  dplyr::filter(dp_str=="DPFALSESNAzNAsigmaNA" | dp_str=="DPTRUES1z0.8sigmaNA") %>%
   ggplot(aes(x=Step, y=Value, group=factor(alpha))) +
-  geom_line(aes(col=alpha)) +
+  geom_line(aes(col=as.numeric(alpha))) +
   facet_grid(rows = vars(factor(test_class)),
              cols = vars(dp_str),
-             labeller=label_parsed) +
+             # labeller=label_parsed
+             ) +
   theme_bw() +
   xlab("Epoch") +
   ylab("Subgroup Loss") +
-  ggtitle(TeX("Convergence of $L_j(\\hat{w}^t)$ on MMNIST")) +
+  ggtitle(TeX("Convergence of $L_j(\\hat{w}^t)$ on MMNIST"),
+          subtitle = "With Clipping") +
   scale_color_continuous(name = alpha_label_name) +
   convergence_plot_theme
-  
 p1
 
+mnist_convergence %>%
+  dplyr::filter(dp_str=="DPFALSESNAzNAsigmaNA" | 
+                  dp_str=="DPTRUESInfzNAsigma0.8" |
+                  dp_str=="DPTRUESInfzNAsigma50") %>%
+  ggplot(aes(x=Step, y=Value, group=factor(alpha))) +
+  geom_line(aes(col=as.numeric(alpha))) +
+  facet_grid(rows = vars(factor(test_class)),
+             cols = vars(dp_str),
+             # labeller=label_parsed
+  ) +
+  theme_bw() +
+  xlab("Epoch") +
+  ylab("Subgroup Loss") +
+  ggtitle(TeX("Convergence of $L_j(\\hat{w}^t)$ on MMNIST"),
+          subtitle = "With Clipping") +
+  scale_color_continuous(name = alpha_label_name) +
+  convergence_plot_theme
 
 ########################
+mmnist_maxstep = max(mnist_convergence$Step)
 mmnist_subgroup_loss_plot <- mnist_convergence %>%
-  dplyr::filter(Step==119, as.integer(dp_str) < 3) %>%
+  dplyr::filter(Step==mmnist_maxstep, 
+                dp_str=="DPFALSESNAzNAsigmaNA" | dp_str=="DPTRUES1z0.8sigmaNA") %>%
   ggplot(aes(x=alpha, y=Value, col=test_class)) + 
-  geom_line() +
+  geom_line(aes(group=test_class)) +
   geom_point() +
   facet_grid(
     cols = vars(dp_str),
-    labeller=label_parsed) +
+    # labeller=label_parsed
+    ) +
   theme_bw() +
   xlab(TeX("Majority Fraction $\\alpha$")) +
   ylab(TeX("Subgroup Loss $L_j$")) + 
@@ -88,11 +111,31 @@ mmnist_subgroup_loss_plot <- mnist_convergence %>%
         legend.position = "bottom") +
   ggtitle("Subgroup Losses on MMNIST")
 mmnist_subgroup_loss_plot
-#######################
 
-# mmnist_arranged <- ggarrange(p2, p1, nrow=1, ncol=2, common.legend = TRUE, legend="bottom")
-# annotate_figure(mmnist_arranged, top=text_grob("MMNIST", size=24))
-# ggsave("./convergence_loss_mnist_subset.pdf", device="pdf", width=8, height=5.25)
+mnist_convergence %>%
+  dplyr::filter(Step==mmnist_maxstep, 
+                dp_str=="DPFALSESNAzNAsigmaNA" | 
+                  dp_str=="DPTRUESInfzNAsigma0.8" |
+                  dp_str=="DPTRUESInfzNAsigma50") %>%
+  ggplot(aes(x=alpha, y=Value, col=test_class)) + 
+  geom_line(aes(group=test_class)) +
+  geom_point() +
+  facet_wrap(. ~ dp_str,
+    # labeller=label_parsed
+    scales = "free"
+  ) +
+  theme_bw() +
+  xlab(TeX("Majority Fraction $\\alpha$")) +
+  ylab(TeX("Subgroup Loss $L_j$")) + 
+  scale_color_grey(name="Subgroup Loss",
+                   labels=c("Loss on Minority", "Loss on Majority")) +
+  theme(plot.title=element_text(hjust=0.5, size=rel(2)),
+        strip.text = element_text(size=rel(1.25)), # size of the facet label text
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        legend.position = "bottom") +
+  ggtitle("Subgroup Losses on MMNIST", subtitle="No clipping")
+#######################
 
 mc10_convergence <- read_convergence_data("./data/cifar10-grouped/0-1-vs-9-2", "attr")
 mc10_convergence$test_class = factor(mc10_convergence$test_class)
--- rscripts/utils.R
+++ rscripts/utils.R
@@ -37,26 +37,37 @@ base_breaks <- function(n = 10){
 }
 
 
+read_file_and_parse_dp_columns <- function(f, loss_group){
+  df = read.csv(f)
+  df$uid = f
+  
+  # Casting of string "None" to numeric will produce "Warning message: NAs introduced by coercion"
+  # which can be safely ignored, so we suppress this warning.
+  df$S = suppressWarnings(as.numeric(sub("-S", "", stringr::str_extract(f, "-S[a-zA-Z0-9\\.]+"))))
+  df$z = suppressWarnings(as.numeric(sub("-z", "", stringr::str_extract(f, "-z[a-zA-Z0-9\\.]+"))))
+  df$sigma = suppressWarnings(as.numeric(sub("-sigma", "", stringr::str_extract(f, "-sigma[a-zA-Z0-9\\.]+"))))
+  
+  df$dp = as.logical(sub("-dp", "", stringr::str_extract(f, "-dp[a-zA-Z]+")))
+  df$alpha = sub("alpha", "", stringr::str_extract(f, "alpha\\d\\.\\d+"))
+  df$test_class = sub(glue("tag-test_loss_per_{loss_group}_"), "", 
+                      stringr::str_extract(f, glue("tag-test_loss_per_{loss_group}_[01]")))
+  return(df)
+}
+
+add_dp_str_column <- function(df){
+  df$dp_str <- do.call(paste0, list("DP", df$dp, 
+                                         "S", df$S, 
+                                         "z", df$z, "sigma", df$sigma))
+  return(df)
+}
+
 # Read the TensorBoard results CSV files, keeping only the final iteration
 # dirname: the directory to read from.
 # loss_group: the group defined by a valid tag, e.g.: tag-test_loss_per_{loss_group}_
 read_data <- function(dirname, loss_group){
   tmp = list()
   for (f in list.files(dirname, full.names = TRUE)){
-    df = read.csv(f)
-    df$uid = f
-    
-    # Casting of string "None" to numeric will produce "Warning message: NAs introduced by coercion"
-    # which can be safely ignored, so we suppress this warning.
-    df$S = suppressWarnings(as.numeric(sub("-S", "", stringr::str_extract(f, "-S[a-zA-Z0-9\\.]+"))))
-    df$z = suppressWarnings(as.numeric(sub("-z", "", stringr::str_extract(f, "-z[a-zA-Z0-9\\.]+"))))
-    df$sigma = suppressWarnings(as.numeric(sub("-sigma", "", stringr::str_extract(f, "-sigma[a-zA-Z0-9\\.]+"))))
-    
-    df$dp = as.logical(sub("-dp", "", stringr::str_extract(f, "-dp[a-zA-Z]+")))
-    df$alpha = sub("alpha", "", stringr::str_extract(f, "alpha\\d\\.\\d+"))
-    df$test_class = sub(glue("tag-test_loss_per_{loss_group}_"), "", 
-                        stringr::str_extract(f, glue("tag-test_loss_per_{loss_group}_[01]")))
-    tmp[[f]] = df
+    tmp[[f]] = read_file_and_parse_dp_columns(f, loss_group)
   }
   results = suppressWarnings(dplyr::bind_rows(tmp))
   maxstep = max(results$Step)
@@ -66,10 +77,7 @@ read_data <- function(dirname, loss_group){
     tidyr::pivot_wider(id_cols=c(alpha, dp, S, z, sigma), 
                        names_from=test_class, values_from=Value) %>%
     dplyr::rename("L_0"="0", "L_1"="1")
-  results$dp_str <- do.call(paste0, list("DP", results$dp, 
-                                         "S", results$S, 
-                                         "z", results$z, "sigma", results$sigma))
-  # levels(results$dp_str) <- c("No DP", "DP-SGD, S = 1, z = 0.8", "DP-SGD, S = 1, z = 1")
+  results <- add_dp_str_column(results)
   return(results)
 }
 
@@ -77,26 +85,15 @@ read_data <- function(dirname, loss_group){
 # dirname: the directory to read from.
 # loss_group: the group defined by a valid tag, e.g.: tag-test_loss_per_{loss_group}_
 read_data_2 <- function(dirname, loss_group){
+  message("[INFO] this function needs updating! It has dp info hard-coded.")
   tmp = list()
   for (f in list.files(dirname, full.names = TRUE, pattern = ".*loss_per_attr_\\d.csv$")){
-    df = read.csv(f)
-    df$uid = f
-    
-    # Casting of string "None" to numeric will produce "Warning message: NAs introduced by coercion"
-    # which can be safely ignored
-    df$S = as.numeric(sub("-S", "", stringr::str_extract(f, "-S[a-zA-Z0-9\\.]+")))
-    df$z = as.numeric(sub("-z", "", stringr::str_extract(f, "-z[a-zA-Z0-9\\.]+")))
-    
-    df$dp = as.logical(sub("-dp", "", stringr::str_extract(f, "-dp[a-zA-Z]+")))
-    df$alpha = sub("alpha", "", stringr::str_extract(f, "alpha\\d\\.\\d+"))
-    df$test_class = sub(glue("tag-test_loss_per_{loss_group}_"), "", 
-                        stringr::str_extract(f, glue("tag-test_loss_per_{loss_group}_[01]")))
-    tmp[[f]] = df
+    tmp[[f]] = read_file_and_parse_dp_columns(f, loss_group)
   }
   results = dplyr::bind_rows(tmp) 
-  results$dp_str = factor(tidyr::replace_na(results$z, 0))
+  results <- add_dp_str_column(results)
   warning("[INFO]: ignoring results for DP runs except: (NoDP; DPSGDS1z0.8)")
-  levels(results$dp_str) <- c("NoDP", "DPSGDS1z0.8", "DPSGDS1z1")
+  # levels(results$dp_str) <- c("NoDP", "DPSGDS1z0.8", "DPSGDS1z1")
   epochs = max(results$Step)
   results <- results %>%
     dplyr::filter(Step == epochs) %>%
@@ -122,23 +119,10 @@ read_data_2 <- function(dirname, loss_group){
 read_convergence_data <- function(dirname, loss_group){
   tmp = list()
   for (f in list.files(dirname, full.names = TRUE)){
-    df = read.csv(f)
-    df$uid = f
-    
-    # Casting of string "None" to numeric will produce "Warning message: NAs introduced by coercion"
-    # which can be safely ignored
-    df$S = as.numeric(sub("-S", "", stringr::str_extract(f, "-S[a-zA-Z0-9\\.]+")))
-    df$z = as.numeric(sub("-z", "", stringr::str_extract(f, "-z[a-zA-Z0-9\\.]+")))
-    
-    df$dp = as.logical(sub("-dp", "", stringr::str_extract(f, "-dp[a-zA-Z]+")))
-    df$alpha = as.numeric(sub("alpha", "", stringr::str_extract(f, "alpha\\d\\.\\d+")))
-    df$test_class = sub(glue("tag-test_loss_per_{loss_group}_"), "", 
-                        stringr::str_extract(f, glue("tag-test_loss_per_{loss_group}_[01]")))
-    tmp[[f]] = df
+    tmp[[f]] = read_file_and_parse_dp_columns(f, loss_group)
   }
   results = dplyr::bind_rows(tmp) %>%
-    select(c("Value", "alpha", "dp", "S", "z", "test_class", "Step"))
-  results$dp_str = factor(tidyr::replace_na(results$z, 0))
-  levels(results$dp_str) <- c("No DP", "DP-SGD, S = 1, z = 0.8", "DP-SGD, S = 1, z = 1")
+    select(c("Value", "alpha", "dp", "S", "z", "sigma", "test_class", "Step"))
+  results <- add_dp_str_column(results)
   return(results)
 }
