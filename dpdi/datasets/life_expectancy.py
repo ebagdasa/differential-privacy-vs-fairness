f82439e0f96fd4887a7b6c64d53727f73109e0a4
jpgard <joshua.patrick.gardner@gmail.com>
2021-05-17 21:33:59 -0700

    bug fix: fix issues with inconsistent column naming in dataset

--- dpdi/datasets/life_expectancy.py
+++ dpdi/datasets/life_expectancy.py
@@ -6,10 +6,11 @@ from dpdi.datasets.utils import normalize_columns
 def load_life_expectancy_dataset(root_dir="../data/life-expectancy", year=2015,
                                  normalize=False):
     data = pd.read_csv(os.path.join(root_dir, 'life-expectancy-data.csv'))
-    data = data[data.Year == year]  # Keep only this year
-    del data['Country']
-    del data['Year']
-    data.rename(columns={'Status': 'sensitive', 'Life expectancy': 'target'})
+    data.columns = [c.strip().lower() for c in data.columns]
+    data = data[data.year == year]  # Keep only this year
+    del data['country']
+    del data['year']
+    data.rename(columns={'status': 'sensitive', 'life expectancy': 'target'}, inplace=True)
     if normalize:
         data = normalize_columns(data)
     return data
