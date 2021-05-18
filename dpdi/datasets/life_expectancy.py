c165aa26e76b6cb4657920ec9d4ecfd0b110da95
jpgard <joshua.patrick.gardner@gmail.com>
2021-05-17 21:37:40 -0700

    add integer casting of the sensitive group

--- dpdi/datasets/life_expectancy.py
+++ dpdi/datasets/life_expectancy.py
@@ -4,12 +4,15 @@ from dpdi.datasets.utils import normalize_columns
 
 
 def load_life_expectancy_dataset(root_dir="../data/life-expectancy", year=2015,
-                                 normalize=False):
+                                 normalize=False, minority_label="Developing"):
     data = pd.read_csv(os.path.join(root_dir, 'life-expectancy-data.csv'))
     data.columns = [c.strip().lower() for c in data.columns]
     data = data[data.year == year]  # Keep only this year
     del data['country']
     del data['year']
+    # Create an integer indicator for minority group' note that the groupings are
+    # "Developing" and "Developed"
+    data['status'] = (data['status'] == minority_label).astype(int)
     data.rename(columns={'status': 'sensitive', 'life expectancy': 'target'}, inplace=True)
     if normalize:
         data = normalize_columns(data)
